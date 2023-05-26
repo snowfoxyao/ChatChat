f1afd2167f743edaca41e8ab9fc9525121a9fdee
snowfoxyao <132799830+snowfoxyao@users.noreply.github.com>
2023-05-26 14:39:02 +0000

    Your commit message

--- config/custom.config.tsx
+++ config/custom.config.tsx
@@ -17,11 +17,7 @@ export const customConfig: customConfig = {
     InputArea: {
         banner: (
             <p className='text-sm'>
-                Copyright © Chat Chat.{' '}
-                <Link href='https://github.com/okisdev/ChatChat' target='_blank' className='underline'>
-                    Open-Source
-                </Link>
-                . AGPL-3.0 License.
+                Copyright © JARVIS AI.{' '}
             </p>
         ),
     },
@@ -29,15 +25,15 @@ export const customConfig: customConfig = {
         footer: (
             <p className='text-xs'>
                 By continuing, you agree to our{' '}
-                <Link href='https://www.harrly.com' target='_blank' className='underline'>
+                <Link href='/' target='_blank' className='underline'>
                     Terms of Service
                 </Link>
                 ,{' '}
-                <Link href='https://www.harrly.com/privacy-policy' target='_blank' className='underline'>
+                <Link href='/' target='_blank' className='underline'>
                     Privacy Policy
                 </Link>{' '}
                 and{' '}
-                <Link href='https://www.harrly.com/cookies' target='_blank' className='underline'>
+                <Link href='/' target='_blank' className='underline'>
                     Cookie Policy
                 </Link>
                 .
@@ -45,14 +41,10 @@ export const customConfig: customConfig = {
         ),
     },
     Dashboard: {
-        side: '© Chat Chat',
+        side: '© JARVIS AI',
         footer: (
             <p className='text-center text-sm'>
-                Copyright © Chat Chat.{' '}
-                <Link href='https://github.com/okisdev/ChatChat' target='_blank' className='underline'>
-                    Open-Source
-                </Link>
-                . AGPL-3.0 License.
+                Copyright © JARVIS AI.{' '}
             </p>
         ),
     },
